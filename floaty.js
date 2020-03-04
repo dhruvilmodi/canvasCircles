3bf3dcf9d769794ca99749c6222171105db438b2
Dhruvil Modi <dhruvilmodi555@gmail.com>
2020-03-03 19:40:25 -0500

    added class Particle

---
+++ floaty.js
@@ -0,0 +1,12 @@
+class Particle{
+    constructor(x,y,directionX, directionY, size, colour, canvas, ctx){
+        this.x = x;
+        this.y = y;
+        this.directionX = directionX;
+        this.directionY = directionY;
+        this.size = size;
+        this.colour = colour;
+        this.canvas = canvas;
+        this.ctx = ctx;
+    }
+}
